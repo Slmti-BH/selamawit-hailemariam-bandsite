2c9c3f9f5d64aa8a410c9ba897a8c9233eea578e
Selamawit Hailemariam <selamatwits@gmail.com>
2021-11-28 16:08:22 -0800

    Add Javascript to display shows dates from API on shows page.

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,154 +1,140 @@
-const shows=[
-    {
-      date: "Mon Sept 06 2021 ",
-      venue: "Ronald Lane",
-      location: "San Francisco, CA",
-      button: "BUY TICKETS"  
-    },
-
-    {
-        date: "Tue Sept 21 2021",
-        venue: "Pier 3 East",
-        location: "San Francisco, CA",
-        button: "BUY TICKETS"  
-    },
-
-    {
-        date: "Fri Oct 15 2021",
-        venue: "View Lounge ",
-        location: "San Francisco, CA",
-        button: "BUY TICKETS"  
-    },
-    {
-        date: "Sat Nov 06 2021",
-        venue: "Hyatt Agency",
-        location: "San Francisco, CA",
-        button: "BUY TICKETS"  
-    },
-    {
-        date: "Fri Nov 26 2021",
-        venue: "Moscow Center",
-        location: "San Francisco, CA",
-        button: "BUY TICKETS"  
-    },
-    {
-        date: "Wed Dec 15 2021",
-        venue: "Press Club",
-        location: "San Francisco, CA",
-        button: "BUY TICKETS"  
-    }
-];
+const api_key = "8cf94fd0-8658-4735-bdef-785096ec2cd7";
+const URL = "https://project-1-api.herokuapp.com/";
 
 // select a spot in the html to put the shows
- let showsList = document.querySelector(".shows");
+const showsList = document.querySelector(".shows");
 
 //  create the title before the other card elements
-let titleElement = document.createElement("h2");
+const titleElement = document.createElement("h2");
 titleElement.classList.add("shows__title");
-titleElement.innerText= "Shows";
+titleElement.innerText = "Shows";
 showsList.appendChild(titleElement);
 
 // create a wrapper div for all the show cards in the showsList
-let showsWrapperElement = document.createElement("div");
+const showsWrapperElement = document.createElement("div");
 showsWrapperElement.classList.add("shows__wrapper");
 
+// get the shows data from API
+axios
+  .get(URL + `showdates?api_key=` + api_key)
+  .then((result) => {
+    const shows = result.data;
+    console.log(result.data);
 
-// function to loop through the shows array and display shows in showsList
-function displayShows(){
-    for (let i= 0; i< shows.length; i++){
-        let show = shows[i];
-        let showCard = createCardElement(show);
-        showsWrapperElement.appendChild(showCard);
-    }
-};
-
-displayShows ();
+    shows.forEach(show => {
+      displayShow(show);
+      
+    });
+  })
+  .catch((error) => {
+    console.log(`There was an ` + error);
+  });
+  
+// function that takes a show and display it
+function displayShow(show){
+  const showCard = createCardElement(show);
+  showsWrapperElement.appendChild(showCard);
+}
 
 // create each show's card
-function createCardElement(show){
+function createCardElement(show) {
+  const cardElement = document.createElement("article");
+  cardElement.classList.add("show-card");
 
-    let cardElement = document.createElement("article");
-    cardElement.classList.add("show-card");
+  const dateDiv = document.createElement("div");
+  dateDiv.classList.add("show-card__flex-container");
 
-    
-    let dateDiv = document.createElement("div");
-    dateDiv.classList.add("show-card__flex-container");
+  const dateElement = document.createElement("h3");
+  dateElement.classList.add("show-card__subtitle");
+  dateElement.innerHTML = "DATE";
+  dateDiv.appendChild(dateElement);
 
-    let dateElement = document.createElement("h3");
-    dateElement.classList.add("show-card__subtitle")
-    dateElement.innerHTML = "DATE";
-    dateDiv.appendChild(dateElement);
+  const showDate = createShowDate(show);
+  dateDiv.appendChild(showDate);
 
-    let showDate = createShowDate(show);
-    dateDiv.appendChild(showDate);
+  const venueDiv = document.createElement("div");
+  venueDiv.classList.add("show-card__flex-container");
 
-    let venueDiv = document.createElement("div");
-    venueDiv.classList.add("show-card__flex-container")
+  const venueElement = document.createElement("h3");
+  venueElement.classList.add("show-card__subtitle");
+  venueElement.innerHTML = "VENUE";
+  venueDiv.appendChild(venueElement);
 
-    
-    let venueElement = document.createElement("h3");
-    venueElement.classList.add("show-card__subtitle")
-    venueElement.innerHTML = "VENUE";
-    venueDiv.appendChild(venueElement);
+  const showVenue = createShowVenue(show);
+  venueDiv.appendChild(showVenue);
 
-    let showVenue = createShowVenue(show);
-    venueDiv.appendChild(showVenue);
+  const locationDiv = document.createElement("div");
+  locationDiv.classList.add("show-card__flex-container");
 
-    let locationDiv = document.createElement("div");
-    locationDiv.classList.add("show-card__flex-container")
+  const showLocation = document.createElement("h3");
+  showLocation.classList.add("show-card__subtitle");
+  showLocation.innerText = "LOCATION";
+  locationDiv.appendChild(showLocation);
 
-    
-    let showLocation = document.createElement("h3");
-    showLocation.classList.add("show-card__subtitle");
-    showLocation.innerText = "LOCATION";
-    locationDiv.appendChild(showLocation);
+  const locationElement = createShowLocation(show);
+  locationDiv.appendChild(locationElement);
 
-    let locationElement = createShowLocation(show);
-    locationDiv.appendChild(locationElement);
-    
-    cardElement.appendChild(dateDiv);
-    cardElement.appendChild(venueDiv);
-    cardElement.appendChild(locationDiv);
-    
-    let shopBtn = document.createElement("button");
-    shopBtn.classList.add("show-card__btn");
-    shopBtn.innerText = "BUY TICKETS";
-    cardElement.appendChild(shopBtn);
-    
-    
-    return cardElement;
+  cardElement.appendChild(dateDiv);
+  cardElement.appendChild(venueDiv);
+  cardElement.appendChild(locationDiv);
 
+  const shopBtn = document.createElement("button");
+  shopBtn.classList.add("show-card__btn");
+  shopBtn.innerText = "BUY TICKETS";
+  cardElement.appendChild(shopBtn);
+
+  return cardElement;
 }
+
 // append all the show cards in showsWrapperElement to where you want to display on html file (showsList)
 showsList.appendChild(showsWrapperElement);
 
 // to create the date of the show card
-function createShowDate(show){
-    let showDate = document.createElement("time");
-    showDate.classList.add("show-card__date");
-    showDate.innerText = show.date;
-    
-    return showDate;
+function createShowDate(show) {
+  const showDate = document.createElement("time");
+  showDate.classList.add("show-card__date");
+  // to formate the date
+  const date = new Date(Number(show.date));
+  const days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const formattedDate = [
+    days[date.getDay()],
+    months[date.getMonth()],
+    date.getDate(),
+    date.getUTCFullYear(),
+  ].join(" ");
+  showDate.innerText = formattedDate;
+
+  return showDate;
 }
 
 // to create the venue of the show card
-function createShowVenue(show){
-    let showVenue = document.createElement("p");
-    showVenue.classList.add("show-card__venue");
-    showVenue.innerText = show.venue;
-    
-    return showVenue;
+function createShowVenue(show) {
+  const showVenue = document.createElement("p");
+  showVenue.classList.add("show-card__venue");
+  showVenue.innerText = show.place;
+
+  return showVenue;
 }
 
 // to create the location of the show card
-function createShowLocation(show){
-    let showLocation = document.createElement("p");
-    showLocation.classList.add("show-card__location");
-    showLocation.innerText = show.location;
-    
-    return showLocation;
+function createShowLocation(show) {
+  const showLocation = document.createElement("p");
+  showLocation.classList.add("show-card__location");
+  showLocation.innerText = show.location;
+
+  return showLocation;
 }
-
-
-
-
